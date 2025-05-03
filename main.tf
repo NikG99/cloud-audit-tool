6736dd8a75461faee8f1c01eb106511877f50133
NikG99 <nicole.george@arcadis.com>
2025-05-03 08:39:09 -0400

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,19 @@
+# Sample Terraform code to deploy IAM roles or Lambda
+resource "aws_iam_role" "lambda_exec" {
+  name = "lambda_exec_role"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
